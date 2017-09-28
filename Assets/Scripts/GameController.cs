da1d1d27100cee11f1438f9d1cfef8fe4660ab00
LJ-Software <loganjones512@gmail.com>
2017-09-28 12:30:03 -0500

    added GameController script

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public struct Board
+	{
+		int n;
+		public ArrayList cells = new ArrayList();
+		public GameObject boardGO;
+
+		public Board(int x)
+		{
+			n = x;
+			cells.Capacity = n*n;
+		}
+	}
+
+	Board gameBoard;
+	System.;
+	int currentPlayer;
+
+	public GameController(int boardSize)
+	{
+		gameBoard = new Board (boardSize);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameBoard.boardGO = Instantiate (Resources.Load("Prefabs/GameBoard") as GameObject);
+		currentPlayer = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void PlayerMove(char playerToken)
+	{
+		
+	}
+
+	void CheckForGameOver(Board board, char PlayerToken)
+	{
+		for (int i = 0; i < board.cells.Capacity; i++) {
+			
+		}
+	}
+}
